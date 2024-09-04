18b8f8a2ecf585f66214eaa27bb11cf0699ea613
A.R. Jibon <atikurrahaman386@gmail.com>
2024-09-05 01:51:32 +0600

    refactor: improve file handling in submitAction with unique naming

--- src/app/send/send-action.ts
+++ src/app/send/send-action.ts
@@ -2,21 +2,20 @@
 
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { revalidatePath } from "next/cache";
-import { join } from "path";
+import path from "path";
+import fs from "fs";
 
 export default async function submitAction(
   data: FormData,
 ): Promise<{ success: true } | { success: false; message: string }> {
-  if (!existsSync(join(process.cwd(), "uploads"))) {
-    mkdirSync(join(process.cwd(), "uploads"));
+  if (!existsSync(path.join(process.cwd(), "uploads"))) {
+    mkdirSync(path.join(process.cwd(), "uploads"));
   }
 
   // @ts-ignore
   for (const f of [...data.values()]) {
     const file = f as unknown as File;
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    writeFileSync(join(process.cwd(), "uploads", file.name), buffer);
+    await handleFile(file);
   }
 
   revalidatePath("/");
@@ -24,4 +23,20 @@ export default async function submitAction(
   return {
     success: true,
   };
+}
+
+async function handleFile(file: File, increament?: number) {
+  let fileName = file.name;
+  if (increament) {
+    fileName = `${path.parse(fileName).name} (${increament})${path.parse(fileName).ext}`;
+  }
+
+  const filePath = path.join(process.cwd(), "uploads", fileName);
+
+  if (fs.existsSync(filePath)) {
+    return await handleFile(file, increament ? increament + 1 : 1);
+  }
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  writeFileSync(filePath, buffer);
 }
