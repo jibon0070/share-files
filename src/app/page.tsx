d9196b26b0bc4437569def1acc7f4868afa3f3bb
A.R. Jibon <atikurrahaman386@gmail.com>
2024-09-04 18:49:49 +0600

    feat: display uploaded files list on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
 import Send from "./send/send";
+import fs from "fs";
+import path from "path";
 
 export default function Home() {
+  const files = fs.readdirSync(path.join(process.cwd(), "uploads"));
+  console.log(files);
+
   return (
-    <main>
-      File Sharing App
+    <main className="p-5 container mx-auto grid gap-5">
+      <h1 className="text-3xl text-center">File Sharing App</h1>
       <div className="flex justify-end">
         <Send />
       </div>
+      <table>
+        <thead>
+          <tr>
+            <th className="border p-2 w-0">#</th>
+            <th className="border p-2">Name</th>
+            <th className="border p-2 w-0">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map((file, i) => (
+            <tr key={file}>
+              <td className="border p-2">{i + 1}</td>
+              <td className="border p-2">{file}</td>
+              <td className="border p-2">
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
