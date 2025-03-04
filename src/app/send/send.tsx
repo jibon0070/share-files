845e98621a3cbbdfe7b9c3c9f9257943d39c1257
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-05 02:31:32 +0600

    Use Axios for file upload with progress tracking
    - Replaced `sendAction` with `axios.post` to upload files via `/send`
    API.
    - Added upload progress indicator.
    - Reset progress and refreshed page on successful upload.

--- src/app/send/send.tsx
+++ src/app/send/send.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ChangeEvent, useRef, useState } from "react";
-import sendAction from "./send-action";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 const buttonClass = "bg-purple-600 px-4 py-1 text-white rounded cursor-pointer";
 
 export default function Send() {
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -25,12 +28,21 @@ export default function Send() {
       formData.append(`file-${i}`, file, file.name);
     }
 
-    const r = await sendAction(formData);
+    const r = await axios
+      .post("/send", formData, {
+        onUploadProgress: (event) => {
+          setProgress((event.progress || 0) * 100);
+        },
+      })
+      .then((r) => r.data);
+
+    setProgress(0);
 
     if (!r.success) {
       alert(r.message);
     } else {
       formRef.current?.reset();
+      router.refresh();
     }
 
     setLoading(false);
@@ -38,6 +50,11 @@ export default function Send() {
 
   return (
     <form ref={formRef}>
+      {!!progress && (
+        <div className="w-screen h-screen fixed top-0 left-0 bg-white flex justify-center items-center">
+          <progress max={100} value={progress} />
+        </div>
+      )}
       <label className={buttonClass}>
         {loading ? <div className="bg-gray-400 h-7 w-10 rounded" /> : <>Send</>}
         <input className="hidden" type="file" onChange={send} multiple={true} />
